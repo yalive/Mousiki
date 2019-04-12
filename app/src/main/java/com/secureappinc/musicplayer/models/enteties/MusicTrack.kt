c9f64bb50d2b958aaadd6d8f324e21e1952beef9
yabahddou <fayssel.yabahddou@alm-solutions.mobi>
2019-04-12 23:30:34 +0200

    add add to fav and share feature

--- app/src/main/java/com/secureappinc/musicplayer/models/enteties/MusicTrack.kt
+++ app/src/main/java/com/secureappinc/musicplayer/models/enteties/MusicTrack.kt
@@ -10,4 +10,8 @@ data class MusicTrack(val youtubeId: String, val title: String, val duration: St
         get() {
             return "https://img.youtube.com/vi/$youtubeId/maxresdefault.jpg"
         }
+    val shareVideoUrl: String
+        get() {
+            return "https://www.youtube.com/watch?v=$youtubeId"
+        }
 }
