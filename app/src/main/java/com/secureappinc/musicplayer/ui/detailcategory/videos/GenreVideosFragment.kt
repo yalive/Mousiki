d8b70f8fc84df04d1504e91d5c3d61d4f9f50ebf
abdelhadi <abdelhadibahddou@gmail.com>
2019-04-16 23:32:10 +0100

    Display image genre

--- app/src/main/java/com/secureappinc/musicplayer/ui/detailcategory/videos/GenreVideosFragment.kt
+++ app/src/main/java/com/secureappinc/musicplayer/ui/detailcategory/videos/GenreVideosFragment.kt
@@ -15,6 +15,7 @@ import com.secureappinc.musicplayer.models.YTCategoryMusictem
 import com.secureappinc.musicplayer.models.enteties.MusicTrack
 import com.secureappinc.musicplayer.ui.MainActivity
 import com.secureappinc.musicplayer.ui.detailcategory.DetailGenreFragment
+import com.secureappinc.musicplayer.ui.detailcategory.DetailGenreViewModel
 import com.secureappinc.musicplayer.ui.home.models.GenreMusic
 import com.secureappinc.musicplayer.utils.gone
 import com.secureappinc.musicplayer.utils.visible
@@ -30,6 +31,7 @@ class GenreVideosFragment : Fragment() {
     lateinit var genreMusic: GenreMusic
 
     lateinit var viewModel: GenreVideosViewModel
+    lateinit var parentViewModel: DetailGenreViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_genre_videos, container, false)
@@ -44,6 +46,8 @@ class GenreVideosFragment : Fragment() {
         }
 
         viewModel = ViewModelProviders.of(this).get(GenreVideosViewModel::class.java)
+        val detailGenreFragment = parentFragment as DetailGenreFragment
+        parentViewModel = ViewModelProviders.of(detailGenreFragment).get(DetailGenreViewModel::class.java)
 
         genreMusic = parcelableGenre
         adapter = GenreVideosAdapter(
@@ -69,6 +73,7 @@ class GenreVideosFragment : Fragment() {
                 recyclerView.visible()
                 progressBar.gone()
                 txtError.gone()
+                parentViewModel.firstTrack.value = resource.data!![0]
             }
             Status.ERROR -> {
                 progressBar.gone()
@@ -83,16 +88,5 @@ class GenreVideosFragment : Fragment() {
 
     private fun laodCategoryMusic() {
         viewModel.loadVideosForTopic(genreMusic.topicId)
-    }
-
-    private fun createTracksListFrom(listMusics: List<YTCategoryMusictem>): List<MusicTrack> {
-        val tracks: MutableList<MusicTrack> = mutableListOf()
-        for (musicItem in listMusics) {
-            val track =
-                MusicTrack(musicItem.id.videoId, musicItem.snippet.title, musicItem.contentDetails.duration)
-            tracks.add(track)
-        }
-        return tracks
-
     }
 }
