c159e9add27592e0895932d9a323a05b26431b7b
abdelhadi <abdelhadibahddou@gmail.com>
2019-04-16 23:32:10 +0100

    Display image genre

--- app/src/main/java/com/secureappinc/musicplayer/ui/detailcategory/DetailGenreFragment.kt
+++ app/src/main/java/com/secureappinc/musicplayer/ui/detailcategory/DetailGenreFragment.kt
@@ -1,15 +1,23 @@
 package com.secureappinc.musicplayer.ui.detailcategory
 
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.secureappinc.musicplayer.R
+import com.secureappinc.musicplayer.models.enteties.MusicTrack
 import com.secureappinc.musicplayer.ui.detailcategory.playlists.GenrePlaylistsFragment
 import com.secureappinc.musicplayer.ui.detailcategory.videos.GenreVideosFragment
 import com.secureappinc.musicplayer.ui.home.models.GenreMusic
+import com.secureappinc.musicplayer.utils.BlurImage
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_detail_genre.*
 
@@ -47,5 +55,32 @@ class DetailGenreFragment : Fragment() {
 
         viewPager.adapter = DetailGenrePagerAdapter(childFragmentManager, listOf(videosFragment, playlistsFragment))
         tabLayout.setupWithViewPager(viewPager)
+
+        val viewModel = ViewModelProviders.of(this).get(DetailGenreViewModel::class.java)
+        viewModel.firstTrack.observe(this, Observer { firstTrack ->
+            /*Picasso.get().load(firstTrack.imgUrl)
+                //.fit()
+                .into(imgProfileGenre)*/
+            loadAndBlureImage(firstTrack)
+        })
+
+        imgProfileGenre.setImageResource(genreMusic.img)
+    }
+
+    private fun loadAndBlureImage(video: MusicTrack) {
+        val target = object : Target {
+            override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+            }
+
+            override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {
+            }
+
+            override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+                blureBackgorundImg.setImageBitmap(BlurImage.fastblur(bitmap, 2f, 90))
+            }
+        }
+
+        blureBackgorundImg.tag = target
+        Picasso.get().load(video.imgUrl).into(target)
     }
 }
