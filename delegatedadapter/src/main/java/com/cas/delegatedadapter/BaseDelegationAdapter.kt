3318bdf93a42a539c3e49c3d55b197054a707e92
abdelhadi <abdelhadibahddou@gmail.com>
2019-12-03 07:38:11 +0100

    Adapter delegation to make it easy to create adapter with multiple view types.

---
+++ delegatedadapter/src/main/java/com/cas/delegatedadapter/BaseDelegationAdapter.kt
@@ -0,0 +1,39 @@
+package com.cas.delegatedadapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.cas.common.delegate.observer
+
+/**
+ ***************************************
+ * Created by Abdelhadi on 2019-12-03.
+ ***************************************
+ */
+open class BaseDelegationAdapter(
+    private val delegates: List<AdapterDelegate<List<DisplayableItem>>>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val delegateManager = AdapterDelegatesManager<List<DisplayableItem>>().apply {
+        delegates.forEach { delegate ->
+            addaDelegate(delegate)
+        }
+    }
+
+    var dataItems: MutableList<DisplayableItem> by observer(mutableListOf()) {
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return delegateManager.onCreateViewHolder(parent, viewType)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return delegateManager.getItemViewType(dataItems, position)
+    }
+
+    override fun getItemCount(): Int = dataItems.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        delegateManager.onBindViewHolder(dataItems, position, holder)
+    }
+}
---
+++ delegatedadapter/src/main/java/com/cas/delegatedadapter/DisplayableItem.kt
@@ -0,0 +1,3 @@
+package com.cas.delegatedadapter
+
+interface DisplayableItem
