2043e1fbfa059244f7b60a6ab4cba8c024183f63
abdelhadi <abdelhadibahddou@gmail.com>
2019-12-09 13:49:18 +0100

    Save result search songs to local database.

--- app/src/main/java/com/cas/musicplayer/data/repositories/SearchRepository.kt
+++ app/src/main/java/com/cas/musicplayer/data/repositories/SearchRepository.kt
@@ -1,12 +1,11 @@
 package com.cas.musicplayer.data.repositories
 
-import com.cas.common.result.NO_RESULT
 import com.cas.common.result.Result
-import com.cas.common.result.Result.Success
+import com.cas.common.result.alsoWhenSuccess
+import com.cas.musicplayer.data.datasource.search.SearchLocalDataSource
+import com.cas.musicplayer.data.datasource.search.SearchRemoteDataSource
 import com.cas.musicplayer.data.local.database.dao.SearchQueryDao
 import com.cas.musicplayer.data.local.models.SearchQueryEntity
-import com.cas.musicplayer.data.remote.mappers.*
-import com.cas.musicplayer.data.remote.retrofit.RetrofitRunner
 import com.cas.musicplayer.data.remote.retrofit.YoutubeService
 import com.cas.musicplayer.domain.model.Channel
 import com.cas.musicplayer.domain.model.MusicTrack
@@ -23,51 +22,28 @@ import javax.inject.Singleton
 @Singleton
 class SearchRepository @Inject constructor(
     private var youtubeService: YoutubeService,
-    private val retrofitRunner: RetrofitRunner,
-    private val trackMapper: YTBVideoToTrack,
-    private val playlistMapper: YTBPlaylistToPlaylist,
-    private val channelMapper: YTBChannelToChannel,
-    private val videoIdMapper: YTBSearchResultToVideoId,
-    private val channelIdMapper: YTBSearchResultToChannelId,
-    private val playlistIdMapper: YTBSearchResultToPlaylistId,
+    private val searchRemoteDataSource: SearchRemoteDataSource,
+    private val searchLocalDataSource: SearchLocalDataSource,
     private val searchQueryDao: SearchQueryDao
 ) {
 
     suspend fun searchTracks(query: String): Result<List<MusicTrack>> {
-        val idsResult = retrofitRunner.executeNetworkCall(videoIdMapper.toListMapper()) {
-            youtubeService.searchVideoIdsByQuery(query, 50).items ?: emptyList()
-        } as? Success ?: return NO_RESULT
-
-        // 2 - Get videos
-        val ids = idsResult.data.joinToString { it.id }
-        return retrofitRunner.executeNetworkCall(trackMapper.toListMapper()) {
-            youtubeService.videos(ids).items ?: emptyList()
+        val localResult = searchLocalDataSource.getSearchResultForQuery(query)
+        if (localResult.isNotEmpty()) {
+            return Result.Success(localResult)
+        }
+        return searchRemoteDataSource.searchTracks(query).alsoWhenSuccess {
+            searchLocalDataSource.saveRemoteSearchSongs(query, it)
         }
     }
 
     suspend fun searchPlaylists(query: String): Result<List<Playlist>> {
-        val idsResult = retrofitRunner.executeNetworkCall(playlistIdMapper.toListMapper()) {
-            youtubeService.searchItemIdsByQuery(query, "playlist", 30).items!!
-        } as? Success ?: return NO_RESULT
-
-        // 2 - Get videos
-        val ids = idsResult.data.joinToString { it.id }
-        val videosResult = retrofitRunner.executeNetworkCall(playlistMapper.toListMapper()) {
-            youtubeService.playlists(ids).items!!
-        }
-        return videosResult
+        return searchRemoteDataSource.searchPlaylists(query)
     }
 
 
     suspend fun searchChannels(query: String): Result<List<Channel>> {
-        val idsResult = retrofitRunner.executeNetworkCall(channelIdMapper.toListMapper()) {
-            youtubeService.searchItemIdsByQuery(query, "channel", 15).items ?: emptyList()
-        } as? Success ?: return NO_RESULT
-
-        val ids = idsResult.data.joinToString { it.id }
-        return retrofitRunner.executeNetworkCall(channelMapper.toListMapper()) {
-            youtubeService.channels(ids).items ?: emptyList()
-        }
+        return searchRemoteDataSource.searchChannels(query)
     }
 
     suspend fun getSuggestions(url: String): List<String> {
