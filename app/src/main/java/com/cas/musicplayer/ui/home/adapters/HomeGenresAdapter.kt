a2ca44e61979b2e0c798e3ab241de8ab470bc14f
fayssel yabahddou <faysselyabahddou@gmail.com>
2019-12-10 20:14:45 +0100

    add function to generate random color

--- app/src/main/java/com/cas/musicplayer/ui/home/adapters/HomeGenresAdapter.kt
+++ app/src/main/java/com/cas/musicplayer/ui/home/adapters/HomeGenresAdapter.kt
@@ -1,20 +1,22 @@
 package com.cas.musicplayer.ui.home.adapters
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.navigation.findNavController
-import com.cas.musicplayer.R
 import com.cas.common.adapter.SimpleBaseAdapter
 import com.cas.common.adapter.SimpleBaseViewHolder
-import com.cas.musicplayer.ui.genres.detailgenre.DetailGenreFragment
+import com.cas.musicplayer.R
 import com.cas.musicplayer.domain.model.GenreMusic
+import com.cas.musicplayer.ui.genres.detailgenre.DetailGenreFragment
 import com.cas.musicplayer.utils.AdsOrigin
 import com.cas.musicplayer.utils.RequestAdsLiveData
 import com.cas.musicplayer.utils.Utils
 import com.cas.musicplayer.utils.drawable
+import java.util.*
 
 /**
  ***************************************
@@ -30,11 +32,13 @@ internal class HomeGenresAdapter : SimpleBaseAdapter<GenreMusic, HomeGenreViewHo
 
 internal class HomeGenreViewHolder(val view: View) : SimpleBaseViewHolder<GenreMusic>(view) {
     private val imgCategory: ImageView = view.findViewById(R.id.imgCategory)
+    private val backgroundCategory: ImageView = view.findViewById(R.id.backgroundCategory)
     private val txtTitle: TextView = view.findViewById(R.id.txtTitle)
 
     override fun bind(genreMusic: GenreMusic) {
         txtTitle.text = genreMusic.title
         imgCategory.setImageDrawable(itemView.context.drawable(genreMusic.img))
+        backgroundCategory.setBackgroundColor(color())
         view.findViewById<ViewGroup>(R.id.cardView).setOnClickListener {
             val bundle = Bundle()
             bundle.putParcelable(DetailGenreFragment.EXTRAS_GENRE, genreMusic)
@@ -47,5 +51,10 @@ internal class HomeGenreViewHolder(val view: View) : SimpleBaseViewHolder<GenreM
                 RequestAdsLiveData.value = AdsOrigin("genre")
             }
         }
+    }
+
+    fun color(): Int {
+        val rnd = Random()
+        return Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
     }
 }
