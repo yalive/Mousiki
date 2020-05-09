d987e46c2e57d308df8e6d5d7e30150a55ca7c40
abdelhadi <abdelhadibahddou@gmail.com>
2020-05-09 15:44:06 +0000

    Reload ad when error.

---
+++ app/src/main/java/com/cas/musicplayer/utils/NavigationExt.kt
@@ -0,0 +1,25 @@
+package com.cas.musicplayer.utils
+
+import android.os.Bundle
+import androidx.annotation.IdRes
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigator
+
+/**
+ ***************************************
+ * Created by Y.Abdelhadi on 5/9/20.
+ ***************************************
+ */
+
+fun NavController.navigateSafeAction(
+    @IdRes resId: Int,
+    args: Bundle? = null,
+    navOptions: NavOptions? = null,
+    navExtras: Navigator.Extras? = null
+) {
+    val action = currentDestination?.getAction(resId) ?: graph.getAction(resId)
+    if (action != null && currentDestination?.id != action.destinationId) {
+        navigate(resId, args, navOptions, navExtras)
+    }
+}
