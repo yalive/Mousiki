0ff870579e9dcd89bfa727af773fdc771701b952
abdelhadi <abdelhadibahddou@gmail.com>
2021-01-02 16:45:42 +0100

    Review ads loading: First approach ==> loading 10ads per session. Second approach: Load ads when loading songs list and keep user waiting until ads is ready also cache loaded ads [To be implemented and compare with first approach].

--- app/src/main/java/com/cas/musicplayer/ui/common/ads/AdsHelper.kt
+++ app/src/main/java/com/cas/musicplayer/ui/common/ads/AdsHelper.kt
@@ -16,35 +16,24 @@ import kotlin.coroutines.resume
  ***************************************
  */
 
-@Deprecated("It doesn't use mediation ")
-suspend fun loadMultipleNativeAds(count: Int) =
-    suspendCancellableCoroutine<List<UnifiedNativeAd>> { continuation ->
-        val ads = mutableListOf<UnifiedNativeAd>()
-        lateinit var adLoader: AdLoader
-        adLoader = AdLoader.Builder(
-            MusicApp.get(), MusicApp.get().getString(R.string.admob_native_id)
-        ).forUnifiedNativeAd {
-            ads.add(it)
-            if (!adLoader.isLoading && continuation.isActive) {
-                continuation.resume(ads)
-            }
-        }.withAdListener(object : AdListener() {
-            override fun onAdFailedToLoad(errorCode: Int) {
-                if ((!adLoader.isLoading || count == 1) && continuation.isActive) {
-                    continuation.resume(ads)
-                }
-            }
-        }).withNativeAdOptions(NativeAdOptions.Builder().build()).build()
-        adLoader.loadAds(AdRequest.Builder().build(), count)
+suspend fun loadMultipleNativeAdWithMediation(count: Int): List<UnifiedNativeAd> =
+    withContext(Dispatchers.IO) {
+        val listAsync = mutableListOf<Deferred<UnifiedNativeAd?>>()
+        for (i in 0 until count) {
+            listAsync.add(async { loadSingleNativeAd() })
+        }
+        return@withContext listAsync.awaitAll().filterNotNull()
     }
 
-suspend fun loadSingleNativeAd() =
+private suspend fun loadSingleNativeAd() =
     suspendCancellableCoroutine<UnifiedNativeAd?> { continuation ->
         val adLoader: AdLoader = AdLoader.Builder(
             MusicApp.get(), MusicApp.get().getString(R.string.admob_native_id)
-        ).forUnifiedNativeAd {
+        ).forUnifiedNativeAd { ad ->
             if (continuation.isActive) {
-                continuation.resume(it)
+                continuation.resume(ad)
+            } else {
+                ad.destroy()
             }
         }.withAdListener(object : AdListener() {
             override fun onAdFailedToLoad(errorCode: Int) {
@@ -54,13 +43,4 @@ suspend fun loadSingleNativeAd() =
             }
         }).withNativeAdOptions(NativeAdOptions.Builder().build()).build()
         adLoader.loadAd(AdRequest.Builder().build())
-    }
-
-suspend fun loadMultipleNativeAdWithMediation(count: Int): List<UnifiedNativeAd> =
-    withContext(Dispatchers.IO) {
-        val listAsync = mutableListOf<Deferred<UnifiedNativeAd?>>()
-        for (i in 0 until count) {
-            listAsync.add(async { loadSingleNativeAd() })
-        }
-        return@withContext listAsync.awaitAll().filterNotNull()
     }
