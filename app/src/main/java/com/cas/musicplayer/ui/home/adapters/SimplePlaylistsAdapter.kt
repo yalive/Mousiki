9aeef65b9aa2351f7e7190d25a07cce87b7d458c
fayssel yabahddou <faysselyabahddou@gmail.com>
2021-01-07 13:15:37 +0100

    improve home screen look

--- app/src/main/java/com/cas/musicplayer/ui/home/adapters/SimplePlaylistsAdapter.kt
+++ app/src/main/java/com/cas/musicplayer/ui/home/adapters/SimplePlaylistsAdapter.kt
@@ -2,12 +2,19 @@ package com.cas.musicplayer.ui.home.adapters
 
 import android.view.View
 import android.widget.TextView
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
 import com.cas.common.adapter.SimpleBaseAdapter
 import com.cas.common.adapter.SimpleBaseViewHolder
 import com.cas.common.extensions.onClick
 import com.cas.musicplayer.R
+import com.cas.musicplayer.data.remote.models.Artist
 import com.cas.musicplayer.data.remote.models.mousiki.SimplePlaylist
-import com.cas.musicplayer.utils.dpToPixel
+import com.cas.musicplayer.ui.artists.EXTRAS_ARTIST
+import com.cas.musicplayer.ui.common.songs.AppImage
+import com.cas.musicplayer.ui.common.songs.BaseSongsFragment
+import com.cas.musicplayer.ui.playlist.songs.PlaylistSongsFragment
+import com.cas.musicplayer.utils.*
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_home_compact_playlist.view.*
@@ -39,13 +46,13 @@ class SimplePlaylistsAdapter(
         init {
             view.findViewById<View>(R.id.cardView).onClick {
                 if (adapterPosition >= 0) {
-                    /*val item = items[adapterPosition]
+                    val item = items[adapterPosition]
                     val artist = Artist(item.title, "US", item.playlistId)
                     val bundle = bundleOf(
                         PlaylistSongsFragment.EXTRAS_PLAYLIST_ID to item.playlistId,
                         EXTRAS_ARTIST to artist,
-                        BaseSongsFragment.EXTRAS_ID_FEATURED_IMAGE to AppImage.AppImageRes(
-                            item.featuredImageRes
+                        BaseSongsFragment.EXTRAS_ID_FEATURED_IMAGE to AppImage.AppImageUrl(
+                            item.featuredImage
                         )
                     )
 
@@ -54,8 +61,8 @@ class SimplePlaylistsAdapter(
 
                     if (!Utils.hasShownAdsOneTime) {
                         Utils.hasShownAdsOneTime = true
-                        RequestAdsLiveData.value = AdsOrigin("chart")
-                    }*/
+                        RequestAdsLiveData.value = AdsOrigin("SimplePlaylists")
+                    }
                 }
             }
         }
