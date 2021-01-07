9d5c0db26466356e2bc8f9ff1ead276260ba1e6a
fayssel yabahddou <faysselyabahddou@gmail.com>
2021-01-07 14:19:12 +0100

    fix exit button not showing when no ads is populated

--- app/src/main/java/com/cas/musicplayer/ui/home/ExitApp.kt
+++ app/src/main/java/com/cas/musicplayer/ui/home/ExitApp.kt
@@ -1,10 +1,14 @@
 package com.cas.musicplayer.ui.home
 
 import android.view.View
-import android.widget.*
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
 import androidx.lifecycle.lifecycleScope
-import com.cas.common.extensions.invisible
+import com.cas.common.extensions.gone
 import com.cas.common.extensions.onClick
+import com.cas.common.extensions.visible
 import com.cas.musicplayer.R
 import com.cas.musicplayer.ui.MainActivity
 import com.google.android.gms.ads.formats.MediaView
@@ -12,6 +16,7 @@ import com.google.android.gms.ads.formats.NativeAd
 import com.google.android.gms.ads.formats.UnifiedNativeAd
 import com.google.android.gms.ads.formats.UnifiedNativeAdView
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.button.MaterialButton
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -28,13 +33,10 @@ fun MainActivity.showExitDialog(): BottomSheetDialog {
     dialog.setCancelable(true)
     dialog.setCanceledOnTouchOutside(true)
     dialog.setContentView(view)
-    view.findViewById<ImageButton>(R.id.btnExit).onClick {
-        dialog.dismiss()
-        lifecycleScope.launch {
-            delay(200)
-            finish()
-        }
-    }
+    val btnExit = view.findViewById<MaterialButton>(R.id.btnExit)
+    btnExit.onClick { exitApp(dialog) }
+    val btnExitNoAds = view.findViewById<MaterialButton>(R.id.btnExitNoAds)
+    btnExitNoAds.onClick { exitApp(dialog) }
     dialog.setOnDismissListener {
         if (!isFinishing) {
             adsViewModel.loadExitAd()
@@ -43,6 +45,7 @@ fun MainActivity.showExitDialog(): BottomSheetDialog {
     val adView = view.findViewById<UnifiedNativeAdView>(R.id.ad_view)
     adView.apply {
         mediaView = findViewById<View>(R.id.ad_media) as MediaView
+        mediaView.setImageScaleType(ImageView.ScaleType.CENTER_CROP)
         // Register the view used for each individual asset.
         headlineView = findViewById(R.id.ad_headline)
         bodyView = findViewById(R.id.ad_body)
@@ -56,10 +59,19 @@ fun MainActivity.showExitDialog(): BottomSheetDialog {
     adsViewModel.exitAd?.let { ad ->
         populateNativeAdView(ad, adView)
     } ?: run {
-        adView.invisible()
+        adView.gone()
+        btnExitNoAds.visible()
     }
     dialog.show()
     return dialog
+}
+
+private fun MainActivity.exitApp(dialog: BottomSheetDialog) {
+    dialog.dismiss()
+    lifecycleScope.launch {
+        delay(200)
+        finish()
+    }
 }
 
 fun populateNativeAdView(
