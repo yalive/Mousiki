ed705464aae237d34b220578239450c92085a86d
abdelhadi <abdelhadibahddou@gmail.com>
2021-01-08 14:25:28 +0100

    Lock screen detection.

--- app/src/main/java/com/cas/musicplayer/player/services/LockScreenReceiver.kt
+++ app/src/main/java/com/cas/musicplayer/player/services/LockScreenReceiver.kt
@@ -7,13 +7,17 @@ import android.content.IntentFilter
 import android.graphics.Color
 import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.MediaSessionCompat
+import android.util.Log
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.WhichButton
 import com.afollestad.materialdialogs.actions.getActionButton
 import com.cas.musicplayer.R
+import com.cas.musicplayer.player.PlayerQueue
 import com.cas.musicplayer.player.extensions.isPlaying
+import com.cas.musicplayer.player.extensions.toText
 import com.cas.musicplayer.player.services.MusicPlayerService.Companion.CustomCommand
 import com.cas.musicplayer.ui.MainActivity
+import com.cas.musicplayer.ui.player.TAG_SERVICE
 import com.cas.musicplayer.utils.canDrawOverApps
 import com.cas.musicplayer.utils.color
 import com.cas.musicplayer.utils.windowOverlayTypeOrPhone
@@ -41,15 +45,20 @@ class LockScreenReceiver(
 
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_SCREEN_OFF) {
-            val isPlaying = mediaController.playbackState?.isPlaying ?: false
+            val state = mediaController.playbackState?.toText()
+            Log.d(TAG_SERVICE, "onReceive: screen off, state=$state")
+            val isPlaying = mediaController.playbackState?.isPlaying == true
             if (isPlaying) {
                 shouldShowPopup = true
                 mediaController.transportControls.pause()
             }
+            // Pause player due to Google policy
+            PlayerQueue.pause()
 
             // Disable notification buttons
             mediaController.sendCommand(CustomCommand.DISABLE_NOTIFICATION_ACTIONS, null, null)
         } else if (intent.action == Intent.ACTION_USER_PRESENT && context.canDrawOverApps()) {
+            Log.d(TAG_SERVICE, "onReceive: screen on shouldShowPopup=$shouldShowPopup")
             if (shouldShowPopup) {
                 shouldShowPopup = false
                 MaterialDialog(context).show {
@@ -63,7 +72,10 @@ class LockScreenReceiver(
                     positiveButton(R.string.common_turn_on) {
                         mediaController.transportControls.play()
                         val startLockScreenIntent = Intent(context, MainActivity::class.java)
-                        startLockScreenIntent.putExtra(MainActivity.EXTRAS_FROM_PLAYER_SERVICE, true)
+                        startLockScreenIntent.putExtra(
+                            MainActivity.EXTRAS_FROM_PLAYER_SERVICE,
+                            true
+                        )
                         startLockScreenIntent.putExtra(
                             MainActivity.EXTRAS_OPEN_BATTERY_SAVER_MODE,
                             true
