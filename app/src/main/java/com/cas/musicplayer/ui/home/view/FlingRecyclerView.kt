b76dc9fac4ded193627451dba0e57d7a90d3fe71
abdelhadi <abdelhadibahddou@gmail.com>
2021-01-15 16:29:56 +0100

    Recycler view with custom fling support.

---
+++ app/src/main/java/com/cas/musicplayer/ui/home/view/FlingRecyclerView.kt
@@ -0,0 +1,18 @@
+package com.cas.musicplayer.ui.home.view
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.recyclerview.widget.RecyclerView
+
+class FlingRecyclerView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : RecyclerView(context, attrs, defStyleAttr) {
+
+    override fun fling(velocityX: Int, velocityY: Int): Boolean {
+        return super.fling(velocityX, (velocityY * FLING_SPEED_FACTOR).toInt());
+    }
+
+    companion object {
+        private const val FLING_SPEED_FACTOR = 1.2f
+    }
+}
