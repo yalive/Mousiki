847c6a3c3d0d2fe20bcbdcb1c950dc10fcc5fd04
abdelhadi <abdelhadibahddou@gmail.com>
2021-01-26 00:08:28 +0100

    Add Kotlinx serialization proguard rules.

--- shared/src/commonMain/kotlin/com/mousiki/shared/domain/models/SearchTracksResult.kt
+++ shared/src/commonMain/kotlin/com/mousiki/shared/domain/models/SearchTracksResult.kt
@@ -1,12 +1,13 @@
 package com.mousiki.shared.domain.models
 
+import com.mousiki.shared.domain.result.Result
+
 data class SearchTracksResult(
     val tracks: List<MusicTrack>,
     val token: String? = null,
     val key: String? = null
 )
 
-/*
 fun Result<SearchTracksResult>.hasData(): Boolean {
     return (this is Result.Success && data.tracks.isNotEmpty())
-}*/
+}
