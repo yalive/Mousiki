3af9002f887bc82c2112c5e60fb9c5d6acc71f9b
abdelhadi <abdelhadibahddou@gmail.com>
2021-06-14 21:24:34 +0100

    Log screen view.

--- shared/src/commonMain/kotlin/com/mousiki/shared/utils/AnalyticsApi.kt
+++ shared/src/commonMain/kotlin/com/mousiki/shared/utils/AnalyticsApi.kt
@@ -2,6 +2,7 @@ package com.mousiki.shared.utils
 
 interface AnalyticsApi {
     fun logEvent(name: String, params: Map<String, Any> = emptyMap())
+    fun logScreenView(screenName: String)
 }
 
 fun AnalyticsApi.logEvent(name: String, vararg params: Pair<String, Any>) {
