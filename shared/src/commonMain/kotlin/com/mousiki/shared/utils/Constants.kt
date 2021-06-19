534885edee395fa64b8815c0a5218e1e78f9c8b1
fayssel yabahddou <faysselyabahddou@gmail.com>
2021-06-19 18:37:27 +0100

    setup local song view model and repo with koin

--- shared/src/commonMain/kotlin/com/mousiki/shared/utils/Constants.kt
+++ shared/src/commonMain/kotlin/com/mousiki/shared/utils/Constants.kt
@@ -11,4 +11,11 @@ object Constants {
     const val FAV_PLAYLIST_NAME = "Favourite"
 
     const val MUSIC_TRACK_KEY = "MUSIC_TRACK"
+
+    const val SONG_SORT_ORDER = "song_sort_order"
+
+    const val FILTER_SONG = "filter_song"
+
+    const val INITIALIZED_BLACKLIST = "initialized_blacklist"
+
 }
