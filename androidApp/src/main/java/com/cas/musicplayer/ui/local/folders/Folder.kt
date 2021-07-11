af0e9f63ba52526651026fc1069c72852e68321b
fayssel yabahddou <faysselyabahddou@gmail.com>
2021-07-11 11:41:34 +0100

    Add folder details screen

--- androidApp/src/main/java/com/cas/musicplayer/ui/local/folders/Folder.kt
+++ androidApp/src/main/java/com/cas/musicplayer/ui/local/folders/Folder.kt
@@ -9,7 +9,7 @@ import java.io.File
 /**
  * Created by Fayssel Yabahddou on 6/25/21.
  */
-class Folder(
+data class Folder(
     val id: Long = -1,
     val name: String = "",
     val albumId: Long = -1,
@@ -27,6 +27,30 @@ class Folder(
                 songs
             )
         }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Folder
+
+        if (id != other.id) return false
+        if (name != other.name) return false
+        if (albumId != other.albumId) return false
+        if (path != other.path) return false
+        if (!ids.contentEquals(other.ids)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + name.hashCode()
+        result = 31 * result + albumId.hashCode()
+        result = 31 * result + path.hashCode()
+        result = 31 * result + ids.contentHashCode()
+        return result
     }
 
 }
