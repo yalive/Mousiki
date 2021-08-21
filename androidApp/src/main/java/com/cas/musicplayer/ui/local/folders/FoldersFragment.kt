b700875b59043af00b5f965073b6c11f92c34ffe
abdelhadi <abdelhadibahddou@gmail.com>
2021-08-21 23:43:02 +0100

    Hide folder option.

--- androidApp/src/main/java/com/cas/musicplayer/ui/local/folders/FoldersFragment.kt
+++ androidApp/src/main/java/com/cas/musicplayer/ui/local/folders/FoldersFragment.kt
@@ -9,17 +9,15 @@ import com.cas.musicplayer.databinding.FoldersFragmentBinding
 import com.cas.musicplayer.di.Injector
 import com.cas.musicplayer.tmp.observe
 import com.cas.musicplayer.ui.base.BaseFragment
+import com.cas.musicplayer.ui.local.folders.options.FolderOption
 import com.cas.musicplayer.ui.local.songs.StoragePermissionDelegate
 import com.cas.musicplayer.ui.local.songs.StoragePermissionDelegateImpl
+import com.cas.musicplayer.utils.toast
 import com.cas.musicplayer.utils.viewBinding
 
 class FoldersFragment : BaseFragment<FoldersViewModel>(
     R.layout.folders_fragment
 ), StoragePermissionDelegate by StoragePermissionDelegateImpl() {
-
-    companion object {
-        fun newInstance() = FoldersFragment()
-    }
 
     override val screenName: String = "FoldersFragment"
     override val viewModel by viewModel { Injector.foldersViewModel }
@@ -27,7 +25,7 @@ class FoldersFragment : BaseFragment<FoldersViewModel>(
     private val binding by viewBinding(FoldersFragmentBinding::bind)
 
     private val adapter by lazy {
-        FoldersAdapter()
+        FoldersAdapter(::onFolderOption)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -46,4 +44,17 @@ class FoldersFragment : BaseFragment<FoldersViewModel>(
         }
     }
 
+    private fun onFolderOption(folder: Folder, option: FolderOption) {
+        when (option) {
+            FolderOption.Hidden -> {
+                context?.toast(getString(R.string.folder_hidden_message, folder.name))
+                viewModel.loadAllFolders()
+            }
+            else -> Unit
+        }
+    }
+
+    companion object {
+        fun newInstance() = FoldersFragment()
+    }
 }
