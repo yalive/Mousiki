27682822d34e4f95cfd09dec9e45a88f39c4fae8
abdelhadi <abdelhadibahddou@gmail.com>
2021-08-21 09:29:58 +0100

    Revert "Add close icon to notification."
    This reverts commit 3cec9746

--- androidApp/src/main/java/com/cas/musicplayer/player/services/MusicPlayerService.kt
+++ androidApp/src/main/java/com/cas/musicplayer/player/services/MusicPlayerService.kt
@@ -139,12 +139,6 @@ class MusicPlayerService : LifecycleService(), SleepTimer by MusicSleepTimer() {
             val event = intent.getParcelableExtra<KeyEvent>(Intent.EXTRA_KEY_EVENT)
             if (event?.action == KeyEvent.ACTION_DOWN && intent.hasExtra(Intent.EXTRA_PACKAGE_NAME)) {
                 handleLastSessionSysMediaButton()
-            } else if (event?.keyCode == KeyEvent.KEYCODE_MEDIA_STOP) {
-                lifecycleScope.launch {
-                    mediaController.transportControls.pause()
-                    delay(1000) // hack!
-                    stopForeground(true)
-                }
             } else {
                 MediaButtonReceiver.handleIntent(mediaSession, intent)
             }
