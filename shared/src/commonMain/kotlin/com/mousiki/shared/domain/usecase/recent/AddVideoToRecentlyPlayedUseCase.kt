89e8003da2dbccd5554d2b160cb9a4f78006736e
abdelhadi <abdelhadibahddou@gmail.com>
2021-09-08 00:07:18 +0100

    Save recentely played videos to local database.

---
+++ shared/src/commonMain/kotlin/com/mousiki/shared/domain/usecase/recent/AddVideoToRecentlyPlayedUseCase.kt
@@ -0,0 +1,17 @@
+package com.mousiki.shared.domain.usecase.recent
+
+import com.mousiki.shared.data.repository.StatisticsRepository
+import com.mousiki.shared.domain.models.Track
+
+/**
+ ***************************************
+ * Created by Abdelhadi on 2019-11-26.
+ ***************************************
+ */
+class AddVideoToRecentlyPlayedUseCase(
+    private val statisticsRepository: StatisticsRepository
+) {
+    suspend operator fun invoke(track: Track) {
+        statisticsRepository.addVideoToRecent(track)
+    }
+}
