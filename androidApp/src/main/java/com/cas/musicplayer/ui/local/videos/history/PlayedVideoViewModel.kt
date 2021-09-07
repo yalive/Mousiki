b8ebc72e6643550f0cc0bc18a2a269867a72282c
fayssel yabahddou <faysselyabahddou@gmail.com>
2021-09-07 13:54:08 +0100

    Show history of played video #158

---
+++ androidApp/src/main/java/com/cas/musicplayer/ui/local/videos/history/PlayedVideoViewModel.kt
@@ -0,0 +1,49 @@
+package com.cas.musicplayer.ui.local.videos.history
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.cas.musicplayer.ui.local.repository.LocalVideosRepository
+import com.cas.musicplayer.ui.local.repository.filterNotHidden
+import com.mousiki.shared.domain.models.*
+import com.mousiki.shared.player.PlaySongDelegate
+import com.mousiki.shared.player.updateCurrentPlaying
+import com.mousiki.shared.ui.base.BaseViewModel
+import kotlinx.coroutines.*
+
+class PlayedVideoViewModel(
+    private val localSongsRepository: LocalVideosRepository,
+    private val playSongsDelegate: PlaySongDelegate,
+) : BaseViewModel(), PlaySongDelegate by playSongsDelegate {
+
+    private val _playedVideos = MutableLiveData<List<DisplayableItem>>()
+    val playedVideos: LiveData<List<DisplayableItem>>
+        get() = _playedVideos
+
+    init {
+        getAllPlayedVideos()
+    }
+
+    fun getAllPlayedVideos() = viewModelScope.launch {
+        val videos = localSongsRepository.videos().filterNotHidden()
+        val videosItems = videos.map {
+            LocalSong(it).toDisplayedVideoItem()
+        }
+
+        _playedVideos.value = updateCurrentPlaying(videosItems)
+
+    }
+
+    fun onClickTrack(track: Track) = scope.launch {
+        val tracks = _playedVideos.value
+            ?.filterIsInstance<DisplayedVideoItem>()
+            ?.map { it.track } ?: return@launch
+        playTrackFromQueue(track, tracks)
+    }
+
+    fun onPlaybackStateChanged() {
+        val currentItems = _playedVideos.value ?: return
+        val updatedList = updateCurrentPlaying(currentItems)
+        _playedVideos.value = updatedList
+    }
+}
