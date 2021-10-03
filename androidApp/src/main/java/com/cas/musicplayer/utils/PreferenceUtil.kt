436de4d1b3ce2b2a68533bf4a6e455924c382915
abdelhadi <abdelhadibahddou@gmail.com>
2021-10-03 13:33:32 +0100

    Save latest known version to shared pref.

--- androidApp/src/main/java/com/cas/musicplayer/utils/PreferenceUtil.kt
+++ androidApp/src/main/java/com/cas/musicplayer/utils/PreferenceUtil.kt
@@ -3,6 +3,7 @@ package com.cas.musicplayer.utils
 import android.content.SharedPreferences
 import androidx.core.content.edit
 import androidx.preference.PreferenceManager
+import com.cas.musicplayer.BuildConfig
 import com.cas.musicplayer.MusicApp
 import com.mousiki.shared.utils.Constants.ALBUM_ARTISTS_ONLY
 import com.mousiki.shared.utils.Constants.ALBUM_DETAIL_SONG_SORT_ORDER
@@ -22,6 +23,7 @@ import com.mousiki.shared.utils.Constants.FILTER_VIDEO_DURATION
 import com.mousiki.shared.utils.Constants.FILTER_VIDEO_SIZE
 import com.mousiki.shared.utils.Constants.INITIALIZED_BLACKLIST
 import com.mousiki.shared.utils.Constants.MUSIC_SEEN
+import com.mousiki.shared.utils.Constants.PREF_APP_LATEST_VERSION
 import com.mousiki.shared.utils.Constants.SHOW_PIP_DIALOG
 import com.mousiki.shared.utils.Constants.SHOW_VIDEO_GUIDE
 import com.mousiki.shared.utils.Constants.SONG_SORT_ORDER
@@ -187,6 +189,14 @@ object PreferenceUtil {
             true
         )
         set(value) = sharedPreferences.edit { putBoolean(SHOW_VIDEO_GUIDE, value) }
+
+
+    var lastVersion: Int
+        get() = sharedPreferences.getInt(PREF_APP_LATEST_VERSION, -1)
+        set(value) = sharedPreferences.edit { putInt(PREF_APP_LATEST_VERSION, value) }
+
+    val hasNewVersion: Boolean
+        get() = lastVersion != -1 && lastVersion != BuildConfig.VERSION_CODE
 
     fun toggleFolderVisibility(path: String) {
         val hidden = sharedPreferences.getBoolean(path, false)
